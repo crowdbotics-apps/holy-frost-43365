9a0f0625b51228757eac7ce4b643bb3688c52b54
Crowdbotics <team@crowdbotics.com>
2023-09-13 04:58:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-frost-43365",
+  url: "https://holy-frost-43365.botics.co",
+  api: "https://holy-frost-43365.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
